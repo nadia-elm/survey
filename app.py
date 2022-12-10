cbdc4aff1c7bffd2a074c063da05a366e3e8e6df
nadia <nadia.elmfeddel@gmail.com>
2022-12-10 17:29:40 +0400

    commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask,request,render_template,redirect
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 
-# debug = DebugToolbarExtension(app)
+debug = DebugToolbarExtension(app)
 response = []
 
 
@@ -19,13 +19,13 @@ def home_page():
 # def start_survey():
 #     return redirect('/questions/0')
 
-@app.route('/questions', methods=['POST'])
+@app.route('/questions')
 def show_questions():
-    if (len(response) == len(survey.questions)):
-        return redirect('surveyCompletion.html')
+    # if (len(response) == len(survey.questions)):
+    #     return redirect('surveyCompletion.html')
     # question = survey.questions[num]
-    res = request.form.get['someName'].value
-    response.append(res)
+    # res = request.form.get['someName'].value
+    # response.append(res)
     # print(response)
     return render_template('questions.html',survey= survey)
 
