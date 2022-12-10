0083fbbe3026d6a1adf61b6cafca80aa8ea20fb7
nadia <nadia.elmfeddel@gmail.com>
2022-12-10 22:12:36 +0400

    survey working /no sessions yet

--- app.py
+++ app.py
@@ -16,36 +16,49 @@ def home_page():
     return render_template('home.html',survey= survey)
 
 
-# @app.route('start',method=['POST'])
-# def start_survey():
-#     return redirect('/questions/0')
-
-@app.route('/questions')
-def show_questions():
-    # if (len(response) == len(survey.questions)):
-    #     return redirect('surveyCompletion.html')
-    # question = survey.questions[num]
-    # res = request.form.get['someName'].value
-    # response.append(res)
-    # print(response)
-    return render_template('questions.html',survey= survey)
+@app.route('/start',methods=['POST'])
+def start_survey():
+    return redirect('/questions/0')
 
 
-@app.route('/questions/2',methods=['post'])
-def next_question():
+
+@app.route('/answer',methods=['POST'])
+def collect_answers():
     answer = request.form['Name']
     response.append(answer)
-    print(response)
-    return redirect('/questions/2')
+    if(len(response) == len(survey.questions)):
+        return redirect ('/complete')
+    else:
+        return redirect(f"/questions/{len(response)}")
 
 
-@app.route('/questions/2')
-def question2():
+
+
+
+
+@app.route('/questions/<int:n>')
+def show_questions(n):  
+    if (len(response) == len(survey.questions)):
+            return redirect('/complete')
+
+    if(response is None):
+        return redirect('/')
+
+    if(len(response) == len(survey.questions)):
+        return redirect ('/complete')
+
+    current_question = survey.questions[n]
+    return render_template('questions.html', current_question= current_question )
+
+
+@app.route('/complete')
+def complete():
     return render_template('surveyCompletion.html')
 
-@app.route('/answers',methods=['POST'])
-def save_answer():
-    return render_template('answers.html',response= response)
+
+    
+    
+
 
 
 
